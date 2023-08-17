7f5040ae223c810570a56cf501289cdcc3507215
ander <81880091+anderr8@users.noreply.github.com>
2023-08-17 10:59:08 -0300

    Delete next-crud directory
    deletar

--- next-crud/src/core/ClientRepositorio.ts
+++
@@ -1,8 +0,0 @@
-import Client from "./Client";
-
-
-export default interface ClientRepositorio {
-    salvar(client: Client): Promise<Client>
-    excluir(client: Client): Promise<void>
-    obterTodos(): Promise<Client[]>
-}

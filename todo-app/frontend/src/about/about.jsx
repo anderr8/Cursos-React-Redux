67a012616c5da69b6ad0ba2a89132a0a26d2f539
ander <ander.r8@gmail.com>
2023-08-25 11:49:14 -0300

    subir todo

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>São Paulo FC</h2>
+        <p>TRI CAMPEÂO MUNDIAL!!!</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Ganhar a Copa do Brasil.</p>
+
+        <h2>Imprensa</h2>
+        <p>O time tem jogado muito bem.</p>
+    </div>
+)
